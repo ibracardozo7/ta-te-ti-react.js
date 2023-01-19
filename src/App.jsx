ad1125e85f74d8e07b3f9d9da48c8eb6444db0c8
ibracardozo7 <ibracardozo15@gmail.com>
2023-01-19 17:45:02 -0300

    avance

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,53 @@ const TURNS = {
     O: "o"
 }
 
-const board = Array(9).fill(null)
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+
+    const className = `square ${isSelected ? "is-selected" : ""}`
+    return (
+        <div className={className}>
+            {children}
+        </div>
+    )
+}
 
 function App() {
+
+    const [board, setBoard] = useState(Array(9).fill(null))
+    console.log(board)
+
+    const [turn, setTurn] = useState(TURNS.X)
+
+    const updateBoard = () => {
+        
+    }
 
   return (
     <main className='board'>
         <h1>Ta Te Ti</h1>
         <section className='game'>
             {
-            board.map((el, index) => {
+            board.map((_, index) => {
                 return (
-                    <div className='cell'>
-                        <span>
-                        {index}
-                        </span>
-                    </div>
-                    )
+                    <Square
+                        key={index}
+                        index={index}
+                        updateBoard={updateBoard}>
+                            {/* {board[index]} */}
+                    </Square>
+                )
             })
             }
+        </section>
+
+        <section className='turn'>
+            <Square isSelected={turn === TURNS.X}>
+                {TURNS.X}
+            </Square>
+            <Square isSelected={turn === TURNS.O}>
+                {TURNS.O}
+            </Square>
         </section>
     </main>
   )
